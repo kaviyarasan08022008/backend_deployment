ba31c4a8ae342c527354b6e8f9e2c55847580cd9
kavi_offx <kaviyarasan08022008@gmail.com>
2026-03-04 10:36:28 +0530

    engine URL changes

--- api/db/database.py
+++ api/db/database.py
@@ -7,7 +7,10 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:AcademyRootPassword@localhost:5432/village_voice_api")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
 
 SessionLocal = sessionmaker(
     bind=engine,
