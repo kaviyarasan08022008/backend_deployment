8ae002d68d017af5b499e8813fa097ab9e941d3a
kavi_offx <kaviyarasan08022008@gmail.com>
2026-03-04 09:18:03 +0530

    import folder main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from fastapi import FastAPI, Form, File ,UploadFile
-from routers import users, complaints, actions
-from db.database import Base, engine
+from api.routers import users, complaints, actions
+from api.db.database import Base, engine
 from fastapi.middleware.cors import CORSMiddleware
 import cloudinary
 import cloudinary.uploader
