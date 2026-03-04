72cef030a3cb23f415c103c7dcd8bc229008942c
kavi_offx <kaviyarasan08022008@gmail.com>
2026-03-04 09:57:05 +0530

    vercel and database small changes

--- api/db/database.py
+++ api/db/database.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy  import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:AcademyRootPassword@localhost:5432/village_voice_api"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:AcademyRootPassword@localhost:5432/village_voice_api")
 
 engine = create_engine(DATABASE_URL)
 
